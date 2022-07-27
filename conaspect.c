a3b510b0b9142e75e034bebf10c7423f5de379a8
Jon Daniel <jondaniel879@gmail.com>
2022-07-27 23:28:12 +0000

    Add scale multiplier and update Usage

--- conaspect.c
+++ conaspect.c
@@ -31,6 +31,9 @@ int main(int argc, char** argv)
 		vec2i aspect  = { res[0] / ratio[0]  , res[1] / ratio[1]   };
 		vec2i console = { (res[0] >> 3)      , (res[1] >> 4)       };
 
+		if(argc > 3)
+			console[1] *= strtof(argv[3], NULL);
+
 		fprintf(stdout, "MODE: %dx%d %d:%d CONSOLE: %dx%d\n",
 		                res[0], res[1],
 				aspect[0], aspect[1],
@@ -39,6 +42,7 @@ int main(int argc, char** argv)
 		exit(EXIT_SUCCESS);
 	}
 
-	fprintf(stderr, "Usage: %s <width> <height>\n", argv[0]); 
+	fprintf(stderr, "Usage: %s <width> <height> [scale]...\nCalculate aspect ratio and kernel console parameters.\n\n  <width>  - integer screen width\n  <height> - integer screen height\n  [scale]  - optional floating point row multiplier\n\nExamples:\n%s 1024 768\n%s 1024 768 2.0\n\n", argv[0], argv[0], argv[0]);
+
 	exit(EXIT_FAILURE);
 }
